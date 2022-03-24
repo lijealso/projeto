62807d56ebd0c0e9d0bfded9d5bfef57921033a0
lijealso <lijealso@gmail.com>
2022-03-24 18:27:28 +0000

    dados para conexão à base de dados

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+
+app = Flask(__name__)
+
+# Informação para conexão à base de dados
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Inicializar MySQL
+mysql = MySQL(app)
