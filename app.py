f0cc3fd9dab7c8e6496d0b67b0bb3b24dd518aeb
lijealso <lijealso@gmail.com>
2022-03-24 19:19:31 +0000

    Página e função de registo

--- app.py
+++ app.py
@@ -52,3 +52,38 @@ def logout():
    session.pop('username', None)
    # Redirect
    return redirect(url_for('login'))
+
+# http://localhost:5000/register
+# Página de registo
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+def register():
+    # Mensagem de output
+    msg = ''
+    # Verificar se dados estão preenchidos
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Criar variáveis
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        # VErificar se conta existe
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # Se existir, validação / erros
+        if account:
+            msg = 'Conta já existe!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Endereço de email inválido!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Nome de utilizador só deve ter números e caracteres!'
+        elif not username or not password or not email:
+            msg = 'Preencha o formulário!'
+        else:
+            # Se tudo estiver bem, adicionar conta à base de dados
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'Registo efetuado com sucesso!'
+    elif request.method == 'POST':
+        # Formulário vazio
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
